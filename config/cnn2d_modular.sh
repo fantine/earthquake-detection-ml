1e4e94802104b0c0b80d914e461f976b6ab1830b
Fantine Huot <fantine.huot@gmail.com>
2021-03-24 15:26:22 +0100

    Update configuration files

--- config/cnn2d_modular.sh
+++ config/cnn2d_modular.sh
@@ -2,7 +2,10 @@
 
 export MODULE_ARGS=" \
   --model=CNN2DModular \
-  --height=289 \
+  --height=288 \
   --width=512 \
+  --channels=1 \
+  --tfrecord_height=288 \
+  --tfrecord_width=696 \
   --num_epochs=20 \
 "
