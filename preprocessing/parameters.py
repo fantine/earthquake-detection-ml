c0365e3cae0163fdbc3ec3765981a0610ffa97ca
Fantine Huot <fantine.huot@gmail.com>
2021-03-24 10:54:02 +0100

    Preprocess DAS data

---
+++ preprocessing/parameters.py
@@ -0,0 +1,70 @@
+"""Parameters for data preprocessing."""
+
+import datetime
+import os
+import socket
+
+# pylint: disable=invalid-name
+
+# ---- catalog parameters -----------------------------------------------------
+num_noise_examples = 30000
+event_catalog = 'data/event_catalog.pkl'
+noise_catalog = 'data/noise_catalog.pkl'
+
+# ---- datapath ---------------------------------------------------------------
+hostname = socket.gethostname()
+datapath = '/scr1/fantine/'
+project = 'earthquake-detection-ml/'
+if hostname == 'cybele':
+  datapath = '/Users/fantine/data/'
+elif 'cees' in hostname:
+  datapath = '/scratch/fantine/'
+datapath = os.path.join(datapath, project)
+
+
+# ---- USGS seismic network parameters ----------------------------------------
+clientcode = "NCEDC"
+
+starttime = datetime.datetime(year=2016, month=9, day=2)
+endtime = datetime.datetime(year=2019, month=12, day=10)
+
+# Coordinates of the Bay Area
+min_latitude = 37.257  # degrees
+max_latitude = 38.260  # degrees
+min_longitude = -122.577  # degrees
+max_longitude = -121.702  # degrees
+
+min_radius = 0.0  # degrees
+max_radius = 1.0  # degrees
+stanford_latitude = 37.429354  # positive because northern
+stanford_longitude = -122.175839  # negative because western
+stanford_elevation = 29.0  # meters
+
+closest_station = 'JSFB'
+reference_channel = 'EHZ'
+
+network = 'NC'
+stations = 'JSFB,'
+channels = '*'
+location = '*'
+
+# ---- DAS processing parameters ----------------------------------------------
+raw_datapath = os.path.join(datapath, 'raw_data')
+processed_datapath = os.path.join(datapath, 'processed_data')
+
+start_channel = 14
+end_channel = 310
+
+batch = 1000
+minfreq = 1.0  # Hz
+maxfreq = 12.0  # Hz
+# we save time windows from event_time - window//2 to event_time + window//2
+raw_window_length = 60  # seconds
+passive_das_sampling = 50  # samples per second
+clip_percentile = 99.5
+detect_window_length = 20.48
+event_duration = 12
+das_dt = 0.02
+das_downsampling_factor = 2
+channel_subset1 = list(range(13, 301))
+channel_subset2 = list(range(328, 616))
---
+++ preprocessing/process_das.py
@@ -0,0 +1,93 @@
+"""DAS data processing."""
+
+import os
+import random
+
+import h5py
+import numpy as np
+from processing_utils import processing_utils as processing
+
+from preprocessing import parameters
+
+
+random.seed(42)
+
+
+def _process(data, low_freq, high_freq, dt, clip_percentile, q):
+  data = processing.get_strain_rate(data)
+  data = processing.remove_median(data)
+  data = processing.clip(data, clip_percentile)
+  data = processing.bandpass(data, low_freq, high_freq, dt)
+  data = processing.decimate(data, q)
+  data = processing.normalize(data)
+  return data
+
+
+def _crop(data, raw_window, detect_window, event_duration, dt):
+  sps = 1 // dt
+  start_sample = int((raw_window / 2 - (detect_window - event_duration)) * sps)
+  end_sample = int((raw_window / 2 + detect_window) * sps)
+  return data[:, start_sample:end_sample]
+
+
+def _get_label(filename):
+  basename = os.path.basename(filename)
+  if 'noise' in basename:
+    return np.zeros((1,), dtype=np.float32)
+  if 'event' in basename:
+    return np.ones((1,), dtype=np.float32)
+
+
+def write_hdf5(out_file, data, label):
+  with h5py.File(out_file, 'w') as f:
+    f.create_dataset('input', data=data)
+    f.create_dataset('label', data=label)
+
+
+def read_hdf5(filename):
+  with h5py.File(filename, 'r') as f:
+    return f.get('data')[()]
+
+
+def process(file_pattern, in_dir, out_dir, raw_window, detect_window,
+            event_duration, low_freq, high_freq, dt, clip_percentile, q,
+            channel_subset1, channel_subset2):
+  filenames = processing.get_filenames(file_pattern)
+
+  for filename in filenames:
+    data = read_hdf5(filename)
+    data = _process(data, low_freq, high_freq, dt, clip_percentile, q)
+    data = _crop(data, raw_window, detect_window, event_duration, dt * q)
+    label = _get_label(filename)
+    out_file = filename.replace(in_dir, out_dir)
+    data1 = data[channel_subset1]
+    data2 = data[channel_subset2]
+    out_file1 = out_file.replace('.hdf5', '_1.h5')
+    out_file2 = out_file.replace('.hdf5', '_2.h5')
+    write_hdf5(out_file1, data1, label)
+    write_hdf5(out_file2, data2, label)
+
+
+def main():
+  datatype = 'das'
+  datapath = os.path.join(parameters.raw_datapath, datatype)
+  file_pattern = os.path.join(datapath, '*/*/*')
+  process(
+      file_pattern,
+      in_dir=parameters.raw_datapath,
+      out_dir=parameters.processed_datapath,
+      raw_window=parameters.raw_window_length,
+      detect_window=parameters.detect_window_length,
+      event_duration=parameters.event_duration,
+      low_freq=parameters.low_freq,
+      high_freq=parameters.high_freq,
+      dt=parameters.das_dt,
+      clip_percentile=parameters.clip_percentile,
+      q=parameters.das_downsampling_factor,
+      channel_subset1=parameters.channel_subset1,
+      channel_subset2=parameters.channel_subset2
+  )
+
+
+if __name__ == '__main__':
+  main()
